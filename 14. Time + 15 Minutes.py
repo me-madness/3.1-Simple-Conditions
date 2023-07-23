aadbeae385f6470445fbbdcc75080516d1e3d4e3
me_madness <p_rangel@abv.bg>
2023-07-23 14:12:30 +0200

    i need to fix last check

--- 14. Time + 15 Minutes.py
+++ 14. Time + 15 Minutes.py
@@ -8,6 +8,5 @@ if minute > 60:
 if hour >= 24:
         hour = hour - 24
 if minute < 10:
-    print((str(hour) + ':0' + str(minute)))
-    pass        
+    print((str(hour) + ':0' + str(minute)))     
 print(str(hour) + ':' + str(minute))        
