0ff9f8341f722dcdce4d98b1e9ef7983f45e8557
me-madness <58656549+me-madness@users.noreply.github.com>
2023-07-24 22:22:30 +0200

    metric converter

--- 08. Metric Converter.py
+++ 08. Metric Converter.py
@@ -1,4 +1,45 @@
-size = int(input())
+size = float(input())
 metric_in = input().lower()
 metric_out = input().lower()
 
+if metric_in == 'mm':
+    size = size * 1000
+elif metric_in == 'cm':
+    size = size * 100
+elif metric_in == 'mi':
+    size = size / 0.000621371192
+elif metric_in == 'in':
+    size = size * 39.3700787
+elif metric_in == 'km':
+    size = size / 0.001        
+elif metric_in == 'ft':
+    size = size * 3.2808399
+else:
+    size = size * 1.0936133
+ 
+if metric_out == 'mm':
+    size = size * 1000
+elif metric_out == 'cm':
+    size = size * 100
+elif metric_out == 'mi':
+    size = size / 0.000621371192
+elif metric_out == 'in':
+    size = size * 39.3700787
+elif metric_out == 'km':
+    size = size * 0.001        
+elif metric_out == 'ft':
+    size = size * 3.2808399
+else:
+    size = size * 1.0936133     
+
+print(size)
+
+
+
+# 1 meter (m)1000 millimeters (mm)
+# 1 meter (m)100 centimeters (cm)
+# 1 meter (m)0.000621371192 miles (mi)
+# 1 meter (m)39.3700787 inches (in)
+# 1 meter (m)0.001 kilometers (km)
+# 1 meter (m)3.2808399 feet (ft)
+# 1 meter (m)1.0936133 yards (yd)
